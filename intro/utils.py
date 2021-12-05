6486ed25494647a20f746a57bab9796f710ba70e
Geo Jolly <geojollyc@gmail.com>
2021-12-05 21:38:22 +0530

    Codes for training multi layer perceptron and some utils codes

--- intro/utils.py
+++ intro/utils.py
@@ -8,10 +8,11 @@ import torch.nn as nn
 from torch.utils import data
 import torchvision
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
 default_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def load_mnist(batch_size = 128, device=default_device):
+def load_mnist(batch_size = 128):
     """
     Loads the MNIST dataset, transforms it and returns a DataLoader.
     """
@@ -21,3 +22,57 @@ def load_mnist(batch_size = 128, device=default_device):
     train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
     test_loader = data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
     return train_loader, test_loader, train_set, test_set
+
+def train_epoch(net,dataloader,lr=0.01,optimizer=None,loss_fn = nn.NLLLoss()):
+    optimizer = optimizer or torch.optim.Adam(net.parameters(),lr=lr)
+    net.train()
+    total_loss,acc,count = 0,0,0
+    for features,labels in dataloader:
+        optimizer.zero_grad()
+        lbls = labels.to(default_device)
+        out = net(features.to(default_device))
+        loss = loss_fn(out,lbls) #cross_entropy(out,labels)
+        loss.backward()
+        optimizer.step()
+        total_loss+=loss
+        _,predicted = torch.max(out,1)
+        acc+=(predicted==lbls).sum()
+        count+=len(labels)
+    return total_loss.item()/count, acc.item()/count
+
+def validate(net, dataloader,loss_fn=nn.NLLLoss()):
+    net.eval()
+    count,acc,loss = 0,0,0
+    with torch.no_grad():
+        for features,labels in dataloader:
+            lbls = labels.to(default_device)
+            out = net(features.to(default_device))
+            loss += loss_fn(out,lbls) 
+            pred = torch.max(out,1)[1]
+            acc += (pred==lbls).sum()
+            count += len(labels)
+    return loss.item()/count, acc.item()/count
+
+def train(net,train_loader,test_loader,optimizer=None,lr=0.01,epochs=10,loss_fn=nn.NLLLoss()):
+    optimizer = optimizer or torch.optim.Adam(net.parameters(),lr=lr)
+    res = { 'train_loss' : [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+    for ep in range(epochs):
+        tl,ta = train_epoch(net,train_loader,optimizer=optimizer,lr=lr,loss_fn=loss_fn)
+        vl,va = validate(net,test_loader,loss_fn=loss_fn)
+        print(f"Epoch {ep:2}, Train acc={ta:.3f}, Val acc={va:.3f}, Train loss={tl:.3f}, Val loss={vl:.3f}")
+        res['train_loss'].append(tl)
+        res['train_acc'].append(ta)
+        res['val_loss'].append(vl)
+        res['val_acc'].append(va)
+    return res
+
+def plot_results(hist):
+    plt.figure(figsize=(15,5))
+    plt.subplot(121)
+    plt.plot(hist['train_acc'], label='Training acc')
+    plt.plot(hist['val_acc'], label='Validation acc')
+    plt.legend()
+    plt.subplot(122)
+    plt.plot(hist['train_loss'], label='Training loss')
+    plt.plot(hist['val_loss'], label='Validation loss')
+    plt.legend()
